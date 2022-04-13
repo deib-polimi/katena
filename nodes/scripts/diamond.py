7c70e2a17bc6a8c3ac19daf4fe52ca04dcd71583
Luca Terracciano <terraccianoluca@gmail.com>
2022-04-13 10:00:05 +0200

    feat: WIP

---
+++ nodes/scripts/diamond.py
@@ -0,0 +1,31 @@
+from web3 import Web3
+import argparse
+import json
+import sys
+
+for arg in sys.argv:
+    print(arg)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--network', type=str, default='localhost:8545',
+                    help='network to deploy the smart contract. Can be either a url or a host:port. Default communication is through HTTPS so https:// is omitted.')
+parser.add_argument('--privateKey', type=str, help='private key to sign transactions')
+parser.add_argument('--contractAbi', type=str, help='ABI of the smart contract to deploy')
+
+args = parser.parse_args()
+
+with open(f'contracts/{args.contractAbi}.json') as f:
+    contract_json = json.load(f)
+
+abi = contract_json['abi']
+bytecode = contract_json['bytecode']
+
+w3 = Web3(Web3.HTTPProvider(f'http://{args.network}', request_kwargs={'verify': False}))
+
+signer = w3.eth.account.from_key(args.privateKey)
+w3.eth.default_account = signer.address
+contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+tx_hash = contract.constructor().transact()
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(tx_receipt.contractAddress)
+
