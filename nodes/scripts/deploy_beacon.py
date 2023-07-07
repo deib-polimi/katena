46f3169482082c3d3aa6ac9e7581b52d70a7e82a
claualc <claudia.baz.alvarez@gmail.com>
2023-07-07 10:40:18 +0200

    add: beacon proxy pattern

--- nodes/scripts/deploy_beacon.py
+++ nodes/scripts/deploy_beacon.py
@@ -9,61 +9,34 @@ parser.add_argument('--network', type=str, default='localhost:8545',
                     help='network to deploy the smart contract. Can be either a url or a host:port. Default communication is through HTTPS so https:// is omitted.')
 parser.add_argument('--privateKey', type=str, help='private key to sign transactions')
 parser.add_argument('--contractAbi', type=str, help='ABI of the smart contract to deploy')
-parser.add_argument('--params', nargs='*', help='contract input parameters')
-parser.add_argument('--addressParams', nargs='*', help='contract input address parameters')
 parser.add_argument('--bytecode', type=str, help='contract bytecode')
 
-parser.add_argument('--proxyCount', type=str, help='number of proxies deployed with the beacon')
-parser.add_argument('--proxyAbi', type=str, help='proxy abi')
-parser.add_argument('--proxyBytecode', type=str, help='proxy byte code')
+parser.add_argument('--beaconAddress', type=str, help='address of the beacon')
+parser.add_argument('--proxyCount', type=int, help='number of proxies to add to the beacon')
 
 args = parser.parse_args()
 
 w3 = Web3(Web3.HTTPProvider(f'http://{args.network}', request_kwargs={'verify': False}))
 
-with open(f'contracts-example/{args.contractAbi}.json') as f:
-    beacon_contract_json = json.load(f)
-
-BEACON_ABI = beacon_contract_json['abi']
-BEACON_BYTECODE = args.bytecode
-
-with open(f'contracts-example/{args.proxyAbi}.json') as f:
+with open(f'contracts/{args.contractAbi}.json') as f:
     proxy_contract_json = json.load(f)
 
 PROXY_ABI = proxy_contract_json['abi']
-PROXY_BYTECODE = args.proxyBytecode
-
+PROXY_BYTECODE = args.bytecode
 
 # BUILD BEACON AND CONTRACT PROXIES
-
 # TO SIGN ALL THE TRANSACTIONS
 signer = w3.eth.account.from_key(args.privateKey.upper())
 w3.eth.default_account = w3.toChecksumAddress(signer.address)
 
 # DEPLOY THE BEACON CONTRACT
-beaconContract = w3.eth.contract(abi=BEACON_ABI, bytecode=BEACON_BYTECODE)
-
-constructor_inputs = []
-for function in beaconContract.abi:
-    if function['type'] == 'constructor':
-        constructor_inputs = function['inputs']
-        break
-
-params = args.params
-address_params = args.addressParams
-casted_params = parse_parameters(constructor_inputs, params, address_params)
-
-tx_hash = beaconContract.constructor(*casted_params).transact()
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-BEACON_ADDRESS = tx_receipt.contractAddress
-
-for i in args.proxyCount:
+count = args.proxyCount
+for i in range(0,count):
     # Deploy proxy contracts
-    proxy_contract_1 = w3.eth.contract(abi=PROXY_ABI, bytecode=PROXY_BYTECODE)
-    tx_hash = beaconContract.constructor(*casted_params).transact()
+    proxy = w3.eth.contract(abi=PROXY_ABI, bytecode=PROXY_BYTECODE)
+    tx_hash = proxy.constructor(w3.toChecksumAddress(args.beaconAddress), b'').transact()
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    print(f'DEPLOYED PROXIE CONTRACT {i} WITH ADDRS: {tx_receipt.contractAddress}')
-
+    print(f'{tx_receipt.contractAddress}')
 
 # # Update the beacon value through the first proxy contract
 # proxy_instance_1 = w3.eth.contract(address=proxy_contract_address_1, abi=proxy_contract_abi)
